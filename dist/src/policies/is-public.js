a602181c4102c78a2949d0835bd1d966cd3dde59
MonsieurBot <71845656+monsieurbot@users.noreply.github.com>
2025-10-28 18:24:16 +0100

    sza

--- dist/src/policies/is-public.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-/**
- * Policy pour permettre l'accès public aux routes n8n
- */
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = (policyContext, config, { strapi }) => {
-    // Toujours autoriser l'accès
-    return true;
-};
